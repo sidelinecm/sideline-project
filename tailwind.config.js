c3dd617f76e7cc34302cd03c0d709b3abdb33ac7
sidelinecm <ploycnx192@gmail.com>
2025-08-01 20:15:22 +0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: [
+    "./**/*.{html,js}", // แก้ไขให้ค้นหาทุกไฟล์ .html และ .js ในทุกโฟลเดอร์ย่อย
+  ],
   theme: {
     extend: {},
   },
