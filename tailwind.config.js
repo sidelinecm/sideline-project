e61dc63a0a6d710ab7632228823c5e97b2a5abda
sidelinecm <ploycnx192@gmail.com>
2025-08-03 00:04:29 +0700

    sidelinePro

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,16 @@ module.exports = {
     './main.js'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'hsl(var(--primary))',
+        'primary-glow': 'hsl(var(--primary-glow))',
+        secondary: 'hsl(var(--secondary))',
+        'secondary-glow': 'hsl(var(--secondary-glow))',
+        foreground: 'hsl(var(--foreground))',
+        'muted-foreground': 'hsl(var(--muted-foreground))',
+      }
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
