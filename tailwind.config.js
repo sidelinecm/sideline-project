123cd3dc000bf7946f8447e45298c748aaa6f59e
sidelinecm <ploycnx192@gmail.com>
2025-08-05 10:50:20 +0700

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./*.{html,js}", // <-- แก้ไข: ให้มองหาไฟล์ .html และ .js ที่ระดับบนสุด
+    "./scr/**/*.{html,js}", // แก้ไข: เปลี่ยนจาก src เป็น scr
   ],
   theme: {
-    extend: {
-      colors: {
-        background: 'hsl(var(--background))',
-        foreground: 'hsl(var(--foreground))',
-        card: 'hsl(var(--card))',
-        primary: 'hsl(var(--primary))',
-        'primary-glow': 'hsl(var(--primary-glow))',
-        secondary: 'hsl(var(--secondary))',
-        muted: 'hsl(var(--muted))',
-        'muted-foreground': 'hsl(var(--muted-foreground))',
-        border: 'hsl(var(--border))',
-        accent: 'hsl(var(--accent))',
-      },
-      fontFamily: {
-        prompt: ['Prompt', 'sans-serif'],
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
