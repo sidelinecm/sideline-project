0000fa24d023c1f11ad4d3c97119954cf18a1121
sidelinecm <ploycnx192@gmail.com>
2026-05-08 10:24:34 +0700

    Add files via upload

--- netlify/edge-functions/ssr-province.js
+++ netlify/edge-functions/ssr-province.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.39.8';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.39.8?target=deno';
 
 const CONFIG = {
     SUPABASE_URL: 'https://zxetzqwjaiumqhrpumln.supabase.co',
